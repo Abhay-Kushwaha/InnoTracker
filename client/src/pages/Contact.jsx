9feaa022bbca4fa96b3f93662968c813448ca743
Abhay-Kushwaha <abhay369kumar@gmail.com>
2025-06-20 23:51:48 +0530

    random pages

---
+++ client/src/pages/Contact.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Contact() {
+  return <div style={{padding: 40}}>Contact Us Page</div>;
+} 
---
+++ client/src/pages/FAQ.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FAQ() {
+  return <div style={{padding: 40}}>FAQ Page</div>;
+} 
